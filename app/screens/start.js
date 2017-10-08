46488615e9f3b6183502f6344ae235969c4c0fec
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-10-07 22:05:48 -0700

    home page styling

--- app/screens/start.js
+++ app/screens/start.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Text, Button, View, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo';
 
 export default class Start extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Button
-          onPress={this.props.scan}
-          title="Scan QR Code"
-          color="#654321"
-        />
+        <LinearGradient
+          colors={['#5693A2', '#57C37B']}
+          start={[0.5, 0]}
+          style={ StyleSheet.create({
+            wrapper: {
+              ...StyleSheet.absoluteFillObject,
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center'
+            }
+          }).wrapper }>
+          <Text style={styles.title}>Bitcoin Security Assessment</Text>
+          <Button
+            onPress={this.props.scan}
+            title="Scan QR Code"
+            color="#5693A2"
+          />
+        </LinearGradient>
       </View>
     );
   }
@@ -18,15 +32,14 @@ export default class Start extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input: {
-    width: 160,
-    padding: 8,
-    margin: 8,
-    borderColor: '#000',
-    borderRadius: 4,
+  title: {
+    color: '#F8F9F4',
+    fontSize: 40,
+    textAlign: 'center',
+    padding: 20,
+    marginBottom: 40
   }
 });
